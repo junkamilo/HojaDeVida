e2162fcb0d4e01d5a3dbb02460bd2b6c5a602fc7
Juan camilo <juancamilo.campobarrios@gmail.com>
2025-07-22 00:24:32 -0500

    por terminar mi hoja de vida

--- src/views/ProyectoWeb/proyectoWebController.js
+++ src/views/ProyectoWeb/proyectoWebController.js
@@ -1,68 +1,64 @@
 import { createRoleAccordion } from "../../components/createRoleAccordion/createRoleAccordion";
+import inicio from '../../assets/MusicWeb/InicioMusica.png';
+import perfiluser1 from '../../assets/MusicWeb/perfilUsuario1.png';
+import perfiluser2 from '../../assets/MusicWeb/perfilUsuario2.png';
+import perfiluser3 from '../../assets/MusicWeb/perfilUsuario3.png';
+import perfiluserArtista1 from '../../assets/MusicWeb/SubirAlbum.png';
+import perfiluserArtista2 from '../../assets/MusicWeb/misPUblicaciones.png';
+import perfiluserArtista3 from '../../assets/MusicWeb/perfilArtistaUsuario.png';
+import Albumfav from '../../assets/MusicWeb/AlbumesFavoritos.png';
 
 export const proyectoWeb = () =>{
-        const app = document.getElementById("app");
-    
-        // Limpiar contenido anterior (opcional si `loadView` ya lo hace)
-        app.innerHTML = "";
-    
-        // Crear un título o encabezado (opcional)
-        const heading = document.createElement("h2");
-        heading.textContent = "Aplicativo Web Music";
-        app.appendChild(heading);
-    
-        // Crear acordeones
-        const adminAccordion = createRoleAccordion(
-            "adminRole",
-            "Usuario",
-            [
-                "https://via.placeholder.com/150",
-                "https://via.placeholder.com/150"
-            ],
-            [
-                "Gestionar productos del menú",
-                "Ver estadísticas de ventas",
-                "Administrar usuarios"
-            ]
-        );
-    
-        const meseroAccordion = createRoleAccordion(
-            "meseroRole",
-            "Generos Musicales",
-            [
-                "https://via.placeholder.com/150"
-            ],
-            [
-                "Tomar pedidos",
-                "Enviar órdenes a cocina",
-                "Ver menú del día"
-            ]
-        );
-    
-        const cocinaAccordion = createRoleAccordion(
-            "cocinaRole",
-            "Albumes",
-            [],
-            [
-                "Ver pedidos pendientes",
-                "Cambiar estado del pedido",
-                "Marcar como entregado"
-            ]
-        );
-        const meseroAuxiliar = createRoleAccordion(
-            "cocinaRole",
-            "Canciones",
-            [],
-            [
-                "Ver pedidos pendientes",
-                "Cambiar estado del pedido",
-                "Marcar como entregado"
-            ]
-        );
-    
-        // Agregar acordeones al contenedor
-        app.appendChild(adminAccordion);
-        app.appendChild(meseroAccordion);
-        app.appendChild(cocinaAccordion);
-        app.appendChild(meseroAuxiliar);
+    const app = document.getElementById("app");
+    app.innerHTML = "";
+
+    const heading = document.createElement("h2");
+    heading.textContent = "Roles en Proyecto Java";
+    app.appendChild(heading);
+
+    const Inicio = createRoleAccordion(
+        "Inicio",
+        "Inicio del aplicativo web",
+        [
+            { descripcion: "Inicio del aplicativo web", imagen: inicio },
+        ]
+    );
+
+    const Usuario = createRoleAccordion(
+        "Usuario",
+        "Usuario en el aplicativo web",
+        [
+            { descripcion: "Inicio del aplicativo web", imagen: perfiluser1 },
+            { descripcion: "Inicio del aplicativo web", imagen: perfiluser2 },
+            { descripcion: "Inicio del aplicativo web", imagen: perfiluser3 },
+        ]
+    );
+
+    const UsuarioArtista = createRoleAccordion(
+        "Usuario",
+        "Usuario en el aplicativo web",
+        [
+            { descripcion: "Inicio del aplicativo web", imagen: perfiluserArtista1 },
+            { descripcion: "Inicio del aplicativo web", imagen: perfiluserArtista2 },
+            { descripcion: "Inicio del aplicativo web", imagen: perfiluserArtista3 },
+        ]
+    );
+
+        const AlbumFavorito = createRoleAccordion(
+        "Inicio",
+        "Inicio del aplicativo web",
+        [
+            { descripcion: "Inicio del aplicativo web", imagen: Albumfav },
+        ]
+    );
+
+            const GeneroFavorito = createRoleAccordion(
+        "Inicio",
+        "Inicio del aplicativo web",
+        [
+            { descripcion: "Inicio del aplicativo web", imagen: Albumfav },
+        ]
+    );
+
+    app.append(Inicio,Usuario,UsuarioArtista,AlbumFavorito);
 }
